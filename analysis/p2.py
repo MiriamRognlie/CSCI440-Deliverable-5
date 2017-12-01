c77093ba6a09790615eae463b65c5e201552d1f7
MiriamRognlie <miriam.rognlie@gmail.com>
2017-11-30 22:04:05 -0700

    stuffff

--- analysis/p2.py
+++ analysis/p2.py
@@ -1,10 +1,21 @@
 from sqlalchemy import create_engine
+from mpl_toolkits.mplot3d import Axes3D
+
 import pandas
 import numpy
+import matplotlib.pyplot as plt
 
 #Create the MySQL Engine
 engine = create_engine('mysql://root:blue@localhost:3306/imdb')
 conn = engine.connect()
 conn.begin()
-data = pandas.read_sql_query('', conn)
+data = pandas.read_sql_query("select Runtime, GrossProfit from movie where Country = 'USA'", conn)
+data2 = pandas.read_sql_query("select Runtime, Rating from movie", conn)
+#Lets
+#Lets create
+plt.scatter(data["Runtime"], data["GrossProfit"])
+plt.show()
+plt.scatter(data2["Runtime"], data2["Rating"])
+plt.show()
+
 print(data)
--- analysis/p3.py
+++ analysis/p3.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 import pandas
 import numpy
+import matplotlib.pyplot as plt
 
 #Create the MySQL Engine
 engine = create_engine('mysql://root:blue@localhost:3306/imdb')
 conn = engine.connect()
 conn.begin()
-data = pandas.read_sql_query('', conn)
+data = pandas.read_sql_query("", conn)
 print(data)
