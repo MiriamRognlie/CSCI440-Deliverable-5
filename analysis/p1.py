dbdf299ba59352de1004dfcef162baf4e0dd2ab4
Chris Thomas <cthomas@admt.im>
2017-11-30 20:45:08 -0700

    P1 Scatterplot

--- analysis/p1.py
+++ analysis/p1.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
 import pandas
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 
 #Create the MySQL Engine
 engine = create_engine('mysql://root:blue@localhost:3306/imdb')
 conn = engine.connect()
 conn.begin()
-data = pandas.read_sql_query('select Country, count(*) as count from movie group by Country order by count desc;', conn)
+data = pandas.read_sql_query("select Budget, GrossProfit from movie where Country='USA'", conn)
+#Lets
+#Lets create
+plt.scatter(data["Budget"], data["GrossProfit"])
+plt.show()
 print(data)
