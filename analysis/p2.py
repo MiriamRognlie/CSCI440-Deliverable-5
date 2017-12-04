239ffbbcc1b22390fb4be09d96fbfed6a96a945d
Chris Thomas <cthomas@admt.im>
2017-12-03 19:09:54 -0700

    Something

--- analysis/p2.py
+++ analysis/p2.py
@@ -4,6 +4,12 @@ from scipy import stats
 from sklearn import metrics
 from sqlalchemy import create_engine
 from sklearn import linear_model
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # Question 2: Does​ ​the​ ​length​ ​of​ ​a​ ​movie​ ​affect​ ​its​ ​profitability​ ​or​ ​ratings?​ ​Is​ ​there​ ​a​ ​point​ ​where​ ​a movie​ ​is​ ​too​ ​short​ ​or​ ​too​ ​long?​ ​Is​ ​the​ ​optimal​ ​length​ ​of​ ​a​ ​movie​ ​influenced​ ​by​ ​its​ ​genre?
 # Create the MySQL Engine
@@ -12,10 +18,10 @@ conn = engine.connect()
 conn.begin()
 # Run a query so "data" is a table containing the runtime of all movies made in the USA and their profit (calculated as gross -budget)
 data = pandas.read_sql_query(
-    "SELECT Runtime, (cast(GrossProfit AS SIGNED) - cast(Budget AS SIGNED )) AS Profit FROM movie WHERE Country = 'USA' AND GrossProfit IS NOT NULL AND Budget IS NOT NULL and Runtime is not null",
+    "SELECT Runtime, (cast(GrossProfit AS SIGNED) - cast(Budget AS SIGNED )) AS Profit FROM movie WHERE Country = 'USA' AND GrossProfit IS NOT NULL AND Budget IS NOT NULL and Runtime is not null order by Runtime",
     conn)
 # Run a query so "data2" is a table containing the runtime of all movies and their ratings
-data2 = pandas.read_sql_query("SELECT Runtime, Rating FROM movie WHERE Rating IS NOT NULL and Country = 'USA' and Runtime is not null", conn)
+data2 = pandas.read_sql_query("SELECT Runtime, Rating FROM movie WHERE Rating IS NOT NULL and Country = 'USA' and Runtime is not null ORDER BY Runtime", conn)
 # Lets create
 plt.scatter(data["Runtime"], data["Profit"])  # set up a scatter plot to display data found from query
 plt.title("Movie Runtime vs Gross Profit")
@@ -25,20 +31,10 @@ plt.ylabel("Gross Profit (100 million dollars)")
 x = data["Runtime"].reshape(-1, 1)
 y = data["Profit"].reshape(-1, 1)
 
-# Lets do some linear regression on the Runtime/Rating
-regr = linear_model.LinearRegression()  # calculate regression line for this data
-regr.fit(x, y)
-pred = regr.predict(x)
-# The coefficients
-print('Regression Coefficient (Slope): \n',
-      regr.coef_)  # prints out the regression line coefficients from the scatter plot
-print('Pearson\'s Correlation: ', stats.pearsonr(x, y))  # calculates and prints Pearson's correlation coefficient
-#  based on data from the scatter plot
-print('Spearman\'s Correlation: ', stats.spearmanr(x, y))  # calculates and prints Spearman's rank correlation
-# coefficient based on data from the scatter plot
-print("Mean squared error: %.2f" % metrics.mean_squared_error(x, y))  # calculates and prints the mean squared error
-plt.plot(x, pred, color='black',
-         linewidth=2)  # displayes a scatter plot popup window with the data from the queries and regression line
+model = make_pipeline(PolynomialFeatures(3), Ridge())
+model.fit(x, y)
+y_plot = model.predict(x)
+plt.plot(x, y_plot, color='black')
 
 plt.show()
 
